d4c8d11586e14b9794eda65a918c6fa3215c280c
Raphael <raphaelgarnier1997@gmail.com>
2025-04-24 15:17:39 +0200

    creating searchView.js , loadSearchResults function, to fetch query results

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
-const controleRecipes = async () => {
+const controlRecipes = async () => {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
@@ -26,7 +21,25 @@ const controleRecipes = async () => {
     recipeView.renderError();
   }
 };
+
+const controlSearchResult = async () => {
+  try {
+    // get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // load search query results
+    await model.loadSearchResults(query);
+
+    // Render results
+    console.log(model.state.search.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const init = () => {
-  recipeView.addHandlerRender(controleRecipes);
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResult);
 };
 init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -79,7 +79,7 @@ class RecipeView {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
-  ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
+  ${this.#data.ingredients.map(this._generateMarkupIngredient).join('')}
  
   </div>
 
@@ -140,8 +140,8 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  #generateMarkupIngredient() {
-    ing => `
+  _generateMarkupIngredient(ing) {
+    return `
     <li class="recipe__ingredient">
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
